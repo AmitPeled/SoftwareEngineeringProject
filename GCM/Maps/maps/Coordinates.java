f1aabb102ae022ffbfd8fe7a1f57567850e704c1
asaf92 <asaf92@gmail.com>
2019-05-24 16:38:20 +0300

    Added Coordinates object

---
+++ GCM/Maps/maps/Coordinates.java
@@ -0,0 +1,15 @@
+package maps;
+
+/**
+ * Represents map-local coordinates
+ *
+ */
+public final class Coordinates {
+	public final float x;
+	public final float y;
+	
+	public Coordinates(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+}
