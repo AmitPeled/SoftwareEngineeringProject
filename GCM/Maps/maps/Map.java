ce4c9403df8401c4b065aabbc649fbc621e53d07
asaf92 <asaf92@gmail.com>
2019-05-24 18:25:40 +0300

    Added width and height

--- GCM/Maps/maps/Map.java
+++ GCM/Maps/maps/Map.java
@@ -8,18 +8,26 @@ package maps;
 public final class Map {
 	
 	private int id;
+	private float width;
+	private float height;
 	
 	/**
 	 * Creates a new Map instance
 	 * @param id The ID number associated with this map. Used for identification in the database 
 	 *  and helps to find the map image files
-	 * @throws IllegalArgumentException Thrown when id is not a positive number
+	 * @throws IllegalArgumentException Thrown when id, width or height are not positive numbers
 	 */
-	public Map(int id) throws IllegalArgumentException {
+	public Map(int id, float width, float height) throws IllegalArgumentException {
 		if(id <= 0) throw new IllegalArgumentException("id has to be a positive number");
+		if(width <= 0.0f) throw new IllegalArgumentException("width has to be a positive number");
+		if(height <= 0.0f) throw new IllegalArgumentException("height has to be a positive number");
 		
 		this.id = id;
+		this.width = width;
+		this.height = height;
 	}
 	
 	public int getId() { return id; }
+	public float getWidth() { return width; }
+	public float getHeight() { return height; }
 }
--- GCM/Maps/maps/tests/MapTest.java
+++ GCM/Maps/maps/tests/MapTest.java
@@ -8,22 +8,48 @@ import org.junit.jupiter.api.Test;
 import maps.Map;
 
 class MapTest {
+	
 	static final int DEFAULT_ID = 1;
+	static final float DEFAULT_WIDTH = 200;
+	static final float DEFAULT_HEIGHT = 120;
+	
 	private Map map;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		map = new Map(1);
+		map = new Map(1,DEFAULT_WIDTH,DEFAULT_HEIGHT);
 	}
 
 	@Test
-	void constructor_idLessThanOne_ThrowsException() {assertThrows(IllegalArgumentException.class, () -> {
-		new Map(-1);
-	});}
+	void constructor_idLessThanOne_ThrowsException() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Map(-1,DEFAULT_WIDTH,DEFAULT_HEIGHT);
+		});
+	}
 
+	@Test
+	void constructor_widthIsNotPositive_Throws() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Map(DEFAULT_ID, -DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		});
+	}
+
+	@Test
+	void constructor_heightIsNotPositive_Throws() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new Map(DEFAULT_ID, DEFAULT_WIDTH, -DEFAULT_HEIGHT);
+		});
+	}
+	
 	@Test
 	void constructor_idIsSet() {
 		int result = map.getId();
 		assertEquals(DEFAULT_ID, result);
+	}
+	
+	@Test
+	void constructor_widthAndHeightAreSet(){
+		assertEquals(DEFAULT_WIDTH, map.getWidth());
+		assertEquals(DEFAULT_HEIGHT, map.getHeight());
 	}
 }
