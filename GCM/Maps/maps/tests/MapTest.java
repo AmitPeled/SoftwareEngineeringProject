a99cff249a49fc9c934690abc702790c2107259a
asaf92 <asaf92@gmail.com>
2019-05-24 18:50:08 +0300

    Added tests for the overloaded Map constructor

--- GCM/Maps/maps/tests/MapTest.java
+++ GCM/Maps/maps/tests/MapTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import maps.Coordinates;
 import maps.Map;
 
 class MapTest {
@@ -12,6 +13,8 @@ class MapTest {
 	static final int DEFAULT_ID = 1;
 	static final float DEFAULT_WIDTH = 200;
 	static final float DEFAULT_HEIGHT = 120;
+	static final float NON_DEFAULT_COORDINATES_X = 2.5f;
+	static final float NON_DEFAULT_COORDINATES_Y = 11.3f;
 	
 	private Map map;
 	
@@ -51,5 +54,18 @@ class MapTest {
 	void constructor_widthAndHeightAreSet(){
 		assertEquals(DEFAULT_WIDTH, map.getWidth());
 		assertEquals(DEFAULT_HEIGHT, map.getHeight());
+	}
+	
+	@Test
+	void constructor_defaultOffsetIsZero() {
+		assertTrue(map.getOffset().equals(new Coordinates(0f,0f)));
+	}
+	
+	@Test
+	void constructor_offsetIsSet() {
+		Coordinates coordinates = new Coordinates(NON_DEFAULT_COORDINATES_X,NON_DEFAULT_COORDINATES_Y);
+		Map anotherMap = new Map(DEFAULT_ID, DEFAULT_WIDTH, DEFAULT_HEIGHT,coordinates);
+		assertEquals(coordinates, anotherMap.getOffset());
+		assertTrue(coordinates.equals(anotherMap.getOffset()));
 	}
 }
