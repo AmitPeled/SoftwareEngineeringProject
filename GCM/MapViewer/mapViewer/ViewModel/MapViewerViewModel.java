30d4daf374340c93cb9fbc7b4708ee73f62cc4ac
asaf92 <asaf92@gmail.com>
2019-05-26 15:18:02 +0300

    Added View and ViewModel classes

---
+++ GCM/MapViewer/mapViewer/ViewModel/MapViewerViewModel.java
@@ -0,0 +1,10 @@
+package mapViewer.ViewModel;
+
+public final class MapViewerViewModel {
+	static final String TEST_IMAGE_FILE = "testfile1.png";
+	
+	public String getMapPath() {
+		return TEST_IMAGE_FILE;
+	}
+
+}
