6215b5322f3e8edeb698adb4d00b52e46f70c09b
asaf92 <35231408+asaf92@users.noreply.github.com>
2019-05-28 20:51:42 +0300

    Map viewer (#6)
    * Added View and ViewModel classes
    
    * Added Hello World app
    
    * Added a button to the test app
    
    * MapViewer now displaying a test map
    
    * Added dataaccess package

---
+++ GCM/App/testApp/TestApplication.java
@@ -0,0 +1,58 @@
+package testApp;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import mapViewer.View.MapViewerView;
+import mapViewer.ViewModel.MapViewerViewModel;
+
+public final class TestApplication extends Application{
+
+	static final int SCREEN_WIDTH = 400;
+	static final int SCREEN_HEIGHT = 400;
+	
+	private Stage primaryStage;
+	private MapViewerView mapViewerView;
+	
+	public TestApplication() {
+		mapViewerView = new MapViewerView(new MapViewerViewModel());
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// Initiating the debug app
+		Text helloWorldText = new Text("Hello World!");
+		StackPane stackPane = new StackPane();
+		primaryStage.setTitle("GCM Debug App");
+		this.primaryStage = primaryStage;
+		stackPane.getChildren().add(helloWorldText);
+		
+		// Adding a "Switch to Map Viewer" button
+		Button switchToMapViewerButton = new Button();
+		switchToMapViewerButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) { switchToMapViewer(); }
+		});
+		switchToMapViewerButton.setText("Switch to Map-Viewer");
+		stackPane.getChildren().add(switchToMapViewerButton);
+		
+		// Displaying the scene
+		Scene scene = new Scene(stackPane,SCREEN_WIDTH,SCREEN_HEIGHT);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	private void switchToMapViewer(){
+		primaryStage.setScene(mapViewerView.getScene());
+	}
+	
+}
---
+++ GCM/MapViewer/mapViewer/View/MapViewerView.java
@@ -0,0 +1,56 @@
+package mapViewer.View;
+
+import java.lang.reflect.Constructor;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import mapViewer.ViewModel.*;
+
+/**
+ * Contains the view elements of the MapViewer.
+ *
+ */
+public final class MapViewerView {
+	
+	static final int WIDTH = 1280;
+	static final int HEIGHT = 720;
+	
+	private MapViewerViewModel viewModel;
+	private String mapPathString;
+	private Image mapImage;
+	
+	public MapViewerView(MapViewerViewModel viewModel) {
+		this.viewModel = viewModel;
+		this.mapPathString = viewModel.getMapPath();
+		this.mapImage = new Image(mapPathString);
+	}
+	
+	public Scene getScene() {
+		GridPane gridPane = GetPane();
+		Scene scene = new Scene(gridPane, WIDTH, HEIGHT);
+		
+		return scene;
+	}
+	
+	GridPane GetPane() {
+		GridPane gridPane = new GridPane();
+		
+		BackgroundImage backgroundImage = new BackgroundImage(
+				mapImage, 
+				BackgroundRepeat.NO_REPEAT, 
+				BackgroundRepeat.NO_REPEAT, 
+				BackgroundPosition.CENTER, 
+				BackgroundSize.DEFAULT);
+		
+		Background background = new Background(backgroundImage);
+		gridPane.setBackground(background);
+		return gridPane;
+	}
+}
