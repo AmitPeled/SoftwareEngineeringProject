f8479497029407b7490e14d85a88dc1710e1cd5e
asaf92 <asaf92@gmail.com>
2019-06-02 15:22:56 +0300

    Added SearchView.java

---
+++ GCM/App/search/SearchView.java
@@ -0,0 +1,18 @@
+package search;
+
+/**
+ * This class is the view of the search menu
+ * it will have: 
+ * - Text-box where you type the search words
+ * - 3 "Search by" options
+ * - Search button
+ * 
+ * Notice that classes that end with "View" only contain
+ * details regarding the appearance of a scene and do not contain
+ * any logic whatsoever. The logic is being done in the ViewModel 
+ * and the Model classes.
+ * 
+ */
+public final class SearchView {
+
+}
