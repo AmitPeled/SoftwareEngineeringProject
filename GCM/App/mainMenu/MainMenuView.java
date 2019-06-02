6474c6c8104b85185a8974184974b47da121935d
asaf92 <asaf92@gmail.com>
2019-06-02 14:24:19 +0300

    Added main menu view

---
+++ GCM/App/mainMenu/MainMenuView.java
@@ -0,0 +1,18 @@
+package mainMenu;
+
+/**
+ * This is where the Main Menu view will be written
+ * This scene will show the following options:
+ * - Search
+ * - Add City (only show for privileged users)
+ * - Reports (only show for privileged users)
+ * - Logout
+ *
+ * Notice that classes that end with "View" only contain
+ * details regarding the appearance of a scene and do not contain
+ * any logic whatsoever. The logic is being done in the ViewModel 
+ * and the Model classes.
+ */
+public final class MainMenuView {
+
+}
