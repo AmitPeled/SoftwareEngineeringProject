b9303e4a8468da8a9e266d0b48073dc5ae892fa3
asaf92 <35231408+asaf92@users.noreply.github.com>
2019-06-07 15:26:48 +0300

    Login refactor (#16)
    * Moved scene switching to private methods in login controllers

--- GCM/App/login/ForgotUsernameController.java
+++ GCM/App/login/ForgotUsernameController.java
@@ -18,26 +18,18 @@ public class ForgotUsernameController {
 
 	@FXML
 	public void back(ActionEvent event) throws IOException {
-		
 		System.out.println("well, im going back");
-		
-		// going back to log in screen
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		//Parent LoginSceneParent = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/fxml/LoginScene.fxml"));
-		Parent LoginSceneParent = loader.load();
-		Scene LoginSceneScene = new Scene(LoginSceneParent);
-		LoginSceneScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		window.setScene(LoginSceneScene);
-		window.show();
+		returnToLoginScreen(event);
 	}
 
 	@FXML
 	public void sendUsername(ActionEvent event) throws IOException {
 		// need to check valid email -> send random pass to his email (or his actual password)
-		
 		System.out.println("after validate the email -> send mail -> go back to the log in screen");
+		returnToLoginScreen(event);
+	}
+	
+	private void returnToLoginScreen(ActionEvent event) throws IOException {
 		// going back to log in screen
 		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		//Parent LoginSceneParent = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
