0a58f7cf8c8a4fe89cb74f282dbdf96ce0a6e3d3
asaf92 <asaf92@gmail.com>
2019-06-09 12:49:56 +0300

    Removed unnecessary imports

--- GCM/App/login/LoginSceneController.java
+++ GCM/App/login/LoginSceneController.java
@@ -1,18 +1,12 @@
 package login;
 
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import mainApp.Controller;
 import mainApp.GcmClient;
 import mainApp.SceneNames;
-import javafx.scene.Node;
 
 public class LoginSceneController implements Controller {
 
