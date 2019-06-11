62ae98573c721830bc29962ce3fa4d6fa1d9f836
eliyahulevi <aoliko54@gmail.com>
2019-06-11 16:06:41 +0300

    fixed flow of register and login to database and back, meybe need to chnage userinfo name beacuse i used it to register too. change main so all DAO of the same user will be updated to him -> future use of other DAO's .

--- GCM/App/register/RegisterModel.java
+++ GCM/App/register/RegisterModel.java
@@ -1,22 +1,23 @@
 package register;
 
-import javafx.scene.control.Alert;
-import javafx.stage.Window;
 import mainApp.GcmClient;
 import mainApp.SceneNames;
+import users.User;
 
 public class RegisterModel {
 
 	private GcmClient gcmClient;
 
 	public RegisterModel(GcmClient gcmClient) {
-		if(gcmClient == null) throw new IllegalArgumentException("GCM client is null");
-		this.gcmClient = gcmClient; 
+		if (gcmClient == null)
+			throw new IllegalArgumentException("GCM client is null");
+		this.gcmClient = gcmClient;
 	}
 
 	public boolean Validusername(String username) {
 		if (username.length() > 10 || username.length() < 4) {
-			//showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your name");
+			// showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form
+			// Error!", "Please enter your name");
 			return false;
 		}
 
@@ -30,37 +31,50 @@ public class RegisterModel {
 		return true;
 	}
 
-	public boolean validConfirmPassword(String password,String confirmPassword) {
-		if(!password.equals(confirmPassword)) {
+	public boolean validConfirmPassword(String password, String confirmPassword) {
+		if (!password.equals(confirmPassword)) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public boolean validName(String name) {
-		if(name.length()>10||name.length()<3) {
+		if (name.length() > 10 || name.length() < 3) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public boolean validlastName(String lastname) {
-		if(lastname.length()>10||lastname.length()<3) {
+		if (lastname.length() > 10 || lastname.length() < 3) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public boolean validPhoneNumber(String phoneNumber) {
-		if(phoneNumber.length()==10) {
+		if (phoneNumber.length() == 10) {
 			return true;
 		}
 		return false;
 	}
-	
-	
-	public void switchScene(SceneNames scene) { gcmClient.switchScene(scene); }
 
-	public void back() { gcmClient.back(); }
-	
+	public boolean register(String username, String password, User user) {
+		// need to send data to register
+		//NOT SUPPOSED TO BE HERE
+		if (!gcmClient.getUserInfo().register(username, password, user)) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	public void switchScene(SceneNames scene) {
+		gcmClient.switchScene(scene);
+	}
+
+	public void back() {
+		gcmClient.back();
+	}
+
 }
--- GCM/App/userInfo/UserInfo.java
+++ GCM/App/userInfo/UserInfo.java
@@ -1,7 +1,11 @@
 package userInfo;
 
+import users.User;
+
 public interface UserInfo {
 
 	boolean login(String username, String password);
+	
+	boolean register(String username, String password,User user);
 
 }
