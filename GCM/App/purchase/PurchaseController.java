6f02433aebfcabb7be1b2efa9dd3fd9c88a94ea3
eliyahulevi <aoliko54@gmail.com>
2019-06-12 17:44:07 +0300

    updating Purchase files, update the initializer so you can call the purxhase scene, it does NOT function when press purchase.

--- GCM/App/purchase/PurchaseController.java
+++ GCM/App/purchase/PurchaseController.java
@@ -1,7 +1,55 @@
 package purchase;
 
-public class PurchaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import mainApp.SceneNames;
+
+public class PurchaseController implements Initializable {
+
+	private PurchaseModel puchaseModel;
 	
+	public PurchaseController(PurchaseModel model) {
+		this.puchaseModel = model;
+	}
 	
+	@FXML
+	private ComboBox<String> yearPicker, mounthPicker;
+	
+	@FXML
+	private TextField cardnumtxt,cvvtxt,nametxt,lastnametxt;
+
+	ObservableList<String> Monthlist = FXCollections.observableArrayList("01", "02", "03", "04", "05", "06", "07", "08",
+			"09", "10", "11", "12");
+	ObservableList<String> Yearlist = FXCollections.observableArrayList("19", "20", "21", "22", "23", "24", "25");
+
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		mounthPicker.setItems(Monthlist);
+		yearPicker.setItems(Yearlist);
+
+	}
+
+	public void back(ActionEvent event) {
+
+		puchaseModel.back();
+	}
+
+	public void purchase(ActionEvent event) {
+		System.out.println("Purchase Map!");
+		//Need to validate all fileds of buyer.
+		//need to send to mail note about purchase of the user buyer
+		puchaseModel.switchScene(SceneNames.MENU);
+	}
 
 }
