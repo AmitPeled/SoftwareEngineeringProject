b6cdf85547281b0f0043a91826889126a50a2d18
Gabriel <gabrimandler@gmail.com>
2019-06-14 21:33:47 +0300

    update approval reports

--- GCM/App/approvalReports/ApprovalReportsController.java
+++ GCM/App/approvalReports/ApprovalReportsController.java
@@ -1,101 +1,117 @@
-//public class JustDoIt extends Application {
-//
-//    private final TableView<Person> table = new TableView<>();
-//    private final ObservableList<Person> data
-//            = FXCollections.observableArrayList(
-//                    new Person("Jacob", "Smith"),
-//                    new Person("Isabella", "Johnson"),
-//                    new Person("Ethan", "Williams"),
-//                    new Person("Emma", "Jones"),
-//                    new Person("Michael", "Brown")
-//            );
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//
-//    @Override
-//    public void start(Stage stage) {
-//        stage.setWidth(450);
-//        stage.setHeight(500);
-//
-//        TableColumn firstNameCol = new TableColumn("First Name");
-//        firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-//
-//        TableColumn lastNameCol = new TableColumn("Last Name");
-//        lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-//
-//        TableColumn actionCol = new TableColumn("Action");
-//        actionCol.setCellValueFactory(new PropertyValueFactory<>("DUMMY"));
-//
-//        Callback<TableColumn<Person, String>, TableCell<Person, String>> cellFactory
-//                = //
-//                new Callback<TableColumn<Person, String>, TableCell<Person, String>>() {
-//            @Override
-//            public TableCell call(final TableColumn<Person, String> param) {
-//                final TableCell<Person, String> cell = new TableCell<Person, String>() {
-//
-//                    final Button btn = new Button("Just Do It");
-//
-//                    @Override
-//                    public void updateItem(String item, boolean empty) {
-//                        super.updateItem(item, empty);
-//                        if (empty) {
-//                            setGraphic(null);
-//                            setText(null);
-//                        } else {
-//                            btn.setOnAction(event -> {
-//                                Person person = getTableView().getItems().get(getIndex());
-//                                System.out.println(person.getFirstName()
-//                                        + "   " + person.getLastName());
-//                            });
-//                            setGraphic(btn);
-//                            setText(null);
-//                        }
-//                    }
-//                };
-//                return cell;
-//            }
-//        };
-//
-//        actionCol.setCellFactory(cellFactory);
-//
-//        table.setItems(data);
-//        table.getColumns().addAll(firstNameCol, lastNameCol, actionCol);
-//
-//        Scene scene = new Scene(new Group());
-//
-//        ((Group) scene.getRoot()).getChildren().addAll(table);
-//
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public static class Person {
-//
-//        private final SimpleStringProperty firstName;
-//        private final SimpleStringProperty lastName;
-//
-//        private Person(String fName, String lName) {
-//            this.firstName = new SimpleStringProperty(fName);
-//            this.lastName = new SimpleStringProperty(lName);
-//        }
-//
-//        public String getFirstName() {
-//            return firstName.get();
-//        }
-//
-//        public void setFirstName(String fName) {
-//            firstName.set(fName);
-//        }
-//
-//        public String getLastName() {
-//            return lastName.get();
-//        }
-//
-//        public void setLastName(String fName) {
-//            lastName.set(fName);
-//        }
-//
-//    }
-//}
+package approvalReports;
+
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import gcmDataAccess.GcmDAO;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
+import maps.City;
+import maps.Coordinates;
+import maps.Site;
+import maps.Tour;
+import search.CustomListCell;
+import search.MapItem;
+import utility.TextFieldUtility;
+
+public class ApprovalReportsController  implements Initializable {
+	private GcmDAO gcmDAO;
+	@FXML
+	TableView<City> cityTable;
+	@FXML
+	TableColumn<City, String> cityName;
+	@FXML
+	TableColumn<City, String> cityDescription;
+	@FXML
+	TableColumn<City, String> actionTaken;
+	@FXML
+	TableColumn<City, Button> approvalDisapproval;
+	
+	public ApprovalReportsController(GcmDAO gcmDAO) {
+		this.gcmDAO = gcmDAO;
+	}
+	
+	public void setColumns() {
+		
+	        List<City> cityLists = new ArrayList<City>();
+	        cityLists.add(new City(1, "haifa", "this city sucks"));
+	        cityLists.add(new City(2, "tel aviv", "well this city doesn't suck"));
+//	        List<String> cityName = new ArrayList<String>();
+//	        for (City city : cityLists) {
+//		        cityName.add(city.getName());
+//			}
+	        cityName.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getName()));
+	        cityDescription.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getDescription()));
+	        //actionTaken.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getDescription()));
+	        
+	        
+	        Callback<TableColumn<City, Button>, TableCell<City, Button>> cellFactory 
+	        = new Callback<TableColumn<City, Button>, TableCell<City, Button>>() {
+	        	@Override
+		        public TableCell call(final TableColumn<City, Button> param) {
+		            final TableCell<City, Button> cell = new TableCell<City, Button>() {
+		
+		                Button approve = new Button("Approve!");
+		                Button disapprove = new Button("Disapprove!");
+		                
+		                @Override
+		                public void updateItem(Button item, boolean empty) {
+		                    super.updateItem(item, empty);
+		                    if (empty) {
+		                        setGraphic(null);
+		                        setText(null);
+		                    } else {
+		                    	approve.setOnAction(event -> {
+		                        	City city = getTableView().getItems().get(getIndex());
+		                        	System.out.println(city.getId());
+		                        });
+		                    	disapprove.setOnAction(event -> {
+		                        	City city = getTableView().getItems().get(getIndex());
+		                        	System.out.println(city.getId());
+		                        });
+		                    	HBox pane = new HBox(approve, disapprove);
+
+		                        setGraphic(pane);
+		                        setText(null);
+		                    }
+		                }
+		            };
+		            return cell;
+		        }
+	        };
+	    	approvalDisapproval.setCellFactory(cellFactory);
+	        
+	        
+	        ObservableList<City> details = FXCollections.observableArrayList(cityLists);
+	        cityTable.setItems(details);
+	}
+	public void initTableView() {
+		//cityName.setCellFactory(TextFieldTableCell.forTableColumn()) 
+
+	}
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		initTableView();
+		setColumns();
+	}
+
+}
--- GCM/App/approvalReports/Main.java
+++ GCM/App/approvalReports/Main.java
@@ -1,36 +1,36 @@
-//package approvalReports;
-//
-//import java.io.IOException;
-//
-//import gcmDataAccess.GcmDAO;
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.stage.Stage;
-//import utility.TextFieldUtility;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//
-//public class Main extends Application {
-//	
-//	@Override
-//	public void start(Stage primaryStage) throws IOException {
-//		 // constructing our scene
-//		 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/editor/addCity.fxml"));
-//		 GcmDAO gcmDAO = new GcmDAO();
-//		 gcmDAO.login("editor", "editor"); 
-//		 TextFieldUtility utilities = new TextFieldUtility();
-//		 fxmlLoader.setController(new ApprovalReportsController());
-//		 Parent root = fxmlLoader.load();
-//		 Scene scene = new Scene( root ); 
-// 
-//		 // setting the stage
-//		 primaryStage.setScene( scene );
-//		 primaryStage.setTitle( "Search" );
-//
-//		 primaryStage.show();
-//	}
-//	
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
-//}
+package approvalReports;
+
+import java.io.IOException;
+
+import gcmDataAccess.GcmDAO;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import utility.TextFieldUtility;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		 // constructing our scene
+		 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/approvalReports/ApprovalReportsScene.fxml"));
+		 GcmDAO gcmDAO = new GcmDAO();
+		 gcmDAO.login("editor", "editor"); 
+		 TextFieldUtility utilities = new TextFieldUtility();
+		 fxmlLoader.setController(new ApprovalReportsController(gcmDAO));
+		 Parent root = fxmlLoader.load();
+		 Scene scene = new Scene( root ); 
+ 
+		 // setting the stage
+		 primaryStage.setScene( scene );
+		 primaryStage.setTitle( "Search" );
+
+		 primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
