0240647871654929f0c8414aaab5c751c437a324
Amit Peled <amit.peled0@gmail.com>
2019-06-14 21:59:01 +0300

    partial contentmanager
    (cherry picked from commit 29ef6efdbe013998e933b9aff365a6fa10f4cf4b)

--- GCM/Database/database/execution/IGcmContentManagerExecutor.java
+++ GCM/Database/database/execution/IGcmContentManagerExecutor.java
@@ -1,18 +1,46 @@
 package database.execution;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import maps.City;
 import maps.Map;
 import maps.Site;
 import maps.Tour;
 
 public interface IGcmContentManagerExecutor {
-	void approveSiteEdit(Site site);
 
-	void approveCityEdit(City city);
+	/**
+	 * @param action the action to take on the edit. action true is approve, false
+	 *               discard.
+	 */	
+	void actionMapAddEdit(Map map, boolean action)throws SQLException;
+	void actionMapUpdateEdit(Map map, boolean action)throws SQLException;
+	void actionMapDeleteEdit(Map map, boolean action)throws SQLException;
+	
+	void actionCityAddEdit(City city, boolean action)throws SQLException;
+	void actionCityUpdateEdit(City city, boolean action)throws SQLException;
+	void actionCityDeleteEdit(City city, boolean action)throws SQLException;
 
-	void approveMapEdit(Map map);
+	void actionSiteAddEdit(Site site, boolean action)throws SQLException;
+	void actionSiteUpdateEdit(Site site, boolean action)throws SQLException;
+	void actionSiteDeleteEdit(Site site, boolean action)throws SQLException;
 
-	void approveTourEdit(Tour tour);
+	/**
+	 * editors content editons. you can take with EditorDAO the original object before the edition. 
+	 */
+	List<Map> getMapsAddEdits() throws SQLException;
+	List<Map> getMapsUpdateEdits()throws SQLException;
+	List<Map> getMapsDeleteEdits()throws SQLException;
 
-	void editMapPrice(int mapId, double newPrice); // edit map to map with new price
+	List<Site> getSitesAddEdits()throws SQLException;
+	List<Site> getSitesUpdateEdits()throws SQLException;
+	List<Site> getSitesDeleteEdits()throws SQLException;
+
+	List<City> getCitiesAddEdits()throws SQLException;
+	List<City> getCitiesUpdateEdits()throws SQLException;
+	List<City> getCitiesDeleteEdits()throws SQLException;
+
+
+	void editCityPrice(int cityId, double newPrice)throws SQLException;
 }
