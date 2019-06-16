b4e9620d3c14a7a5bc31793d6fffdba7ad99ea7f
asaf92 <35231408+asaf92@users.noreply.github.com>
2019-06-16 11:21:49 +0300

    Revert "Temp" (#67)

--- GCM/App/approvalReports/sitesApprovalReports/SiteTableCell.java
+++ GCM/App/approvalReports/sitesApprovalReports/SiteTableCell.java
@@ -1,33 +1,12 @@
 package approvalReports.sitesApprovalReports;
 
-import approvalReports.cityApprovalReports.CitySubmission;
-import gcmDataAccess.GcmDAO;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.layout.HBox;
-import maps.City;
-import maps.Site;
 
 public class SiteTableCell extends TableCell<SiteSubmission, Button> {
             Button approve = new Button("Approve!");
             Button disapprove = new Button("Disapprove!");
-            GcmDAO gcmDAO = new GcmDAO();
-            
-            public SiteTableCell(GcmDAO gcmDAO){
-            	this.gcmDAO = gcmDAO;
-            }
-            
-            public void takeAction(SiteSubmission siteSubmission, Boolean approve) {
-            	String actionTaken = siteSubmission.getActionTaken();
-            	Site site = siteSubmission.getSite();
-            	if(actionTaken.equals("ADD")) {
-            		gcmDAO.actionSiteAddEdit(site, approve);
-            	}else if(actionTaken.equals("UPDATE")) {
-            		gcmDAO.actionSiteUpdateEdit(site, approve);
-            	}else {
-            		gcmDAO.actionSiteDeleteEdit(site, approve);
-            	}
-            }
             
             @Override
             public void updateItem(Button item, boolean empty) {
@@ -36,13 +15,15 @@ public class SiteTableCell extends TableCell<SiteSubmission, Button> {
                     setGraphic(null);
                     setText(null);
                 } else {
-            		SiteSubmission siteSubmission = getTableView().getItems().get(getIndex());
-
                 	approve.setOnAction(event -> {
-                		takeAction(siteSubmission, true);
+                		// actionCityAddEdit(city, true)
+                		SiteSubmission citySubmission = getTableView().getItems().get(getIndex());
+                    	System.out.println(citySubmission.getSite().getId());
                     });
                 	disapprove.setOnAction(event -> {
-                		takeAction(siteSubmission, false);
+                		// actionCityAddEdit(city, false)
+                		SiteSubmission city = getTableView().getItems().get(getIndex());
+                    	System.out.println(city.getSite().getId());
                     });
                 	HBox pane = new HBox(approve, disapprove);
 
