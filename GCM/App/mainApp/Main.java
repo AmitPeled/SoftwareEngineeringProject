074affadda80166990234f87dd46a4b0385c6496
Amit Peled <amit.peled0@gmail.com>
2019-06-16 23:09:52 +0300

    Added server details receival from console. (#79)

--- GCM/App/mainApp/Main.java
+++ GCM/App/mainApp/Main.java
@@ -6,13 +6,15 @@ import gcmDataAccess.GcmDAO;
 import init.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import serverMetaData.ServerMetaDataHandler;
 import userInfo.UserInfo;
 import userInfo.UserInfoImpl;
 
 public class Main extends Application {
+	static final boolean ReceiveServerDetailsFromConsole = false;
+	static GcmDAO gcmDAO;
 	@Override
 	public void start(Stage primaryStage) {
-		GcmDAO gcmDAO = new GcmDAO();
 		UserDAO userDAO = gcmDAO;
 		CustomerDAO customerDAO = gcmDAO;
 		UserInfo userInfo = new UserInfoImpl(userDAO,customerDAO);
@@ -24,6 +26,9 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
+		if(ReceiveServerDetailsFromConsole)
+			ServerMetaDataHandler.receiveFromConsole(args);
+		gcmDAO = new GcmDAO(ServerMetaDataHandler.getServerHostName(), ServerMetaDataHandler.getServerPortNumer());
 		launch(args);
 	}
 }
