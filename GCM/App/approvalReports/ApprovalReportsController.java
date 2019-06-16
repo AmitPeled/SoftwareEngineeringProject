4781f83b1cfe671f002a379277f8a36ae0a1fe34
asaf92 <35231408+asaf92@users.noreply.github.com>
2019-06-16 13:29:55 +0300

    Revert "fixed exceptions"

--- GCM/App/approvalReports/ApprovalReportsController.java
+++ GCM/App/approvalReports/ApprovalReportsController.java
@@ -3,17 +3,12 @@ package approvalReports;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
-
 import approvalReports.cityApprovalReports.CitySubmission;
 import approvalReports.cityApprovalReports.CityTableCell;
-import approvalReports.mapApprovalReports.MapSubmission;
-import approvalReports.mapApprovalReports.MapTableCell;
 import approvalReports.sitesApprovalReports.SiteSubmission;
 import approvalReports.sitesApprovalReports.SiteTableCell;
-
 import approvalReports.tourApprovalReports.TourSubmission;
 import approvalReports.tourApprovalReports.TourTableCell;
 import gcmDataAccess.GcmDAO;
@@ -25,10 +20,6 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -36,7 +27,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 import mainApp.GcmClient;
 import maps.City;
-import maps.Map;
 import maps.Site;
 import maps.Tour;
 
@@ -76,48 +66,27 @@ public class ApprovalReportsController  implements Initializable {
 	TableColumn<TourSubmission, Button> tourApprovalDisapproval;
 	
 	@FXML
-	TableView<MapSubmission> mapTable;
-	@FXML
-	TableColumn<MapSubmission, String> mapName;
-	@FXML
-	TableColumn<MapSubmission, String> mapDescription;
-	@FXML
-	TableColumn<MapSubmission, String> mapPrice;
-	@FXML
-	TableColumn<MapSubmission, String> mapActionTaken;
-	@FXML
-	TableColumn<MapSubmission, Button> mapApprovalDisapproval;
-	
-	
-	@FXML
 	Button cityReports;
 	@FXML
 	Button siteReports;
 	@FXML
 	Button tourReports;
-	@FXML
-	Button mapReports;
-
 	
 	private List<CitySubmission> citySubmissions;
 	private List<SiteSubmission> siteSubmissions;
 	private List<TourSubmission> tourSubmission;
-	private List<MapSubmission> mapSubmission;
 	private GcmClient gcmClient;
 
 	private ApprovalReportsController(GcmClient gcmClient,
 			GcmDAO gcmDAO, 
 			List<CitySubmission> citySubmissions, 
 			List<SiteSubmission> siteSubmissions, 
-			List<TourSubmission> tourSubmissions, 
-			List<MapSubmission> mapSubmissions) {
+			List<TourSubmission> tourSubmissions) {
 		this.gcmClient = gcmClient;
 		this.gcmDAO = gcmDAO;
 		this.citySubmissions = citySubmissions == null ? new ArrayList<CitySubmission>() : citySubmissions;
 		this.siteSubmissions = siteSubmissions == null ? new ArrayList<SiteSubmission>() : siteSubmissions;
 		this.tourSubmission = tourSubmissions  == null ? new ArrayList<TourSubmission>() : tourSubmissions;
-		this.mapSubmission = mapSubmission  == null ? new ArrayList<MapSubmission>() : mapSubmission;
-
 	}
 	
 	public void initSiteTableView() {
@@ -129,7 +98,7 @@ public class ApprovalReportsController  implements Initializable {
 	        siteApprovalDisapproval.setCellFactory(new Callback<TableColumn<SiteSubmission, Button>, TableCell<SiteSubmission, Button>>() {
 	            @Override
 	            public TableCell<SiteSubmission, Button> call(TableColumn<SiteSubmission, Button> param) {
-	            	return new SiteTableCell(gcmDAO);
+	                return new SiteTableCell();
 	            }
 	        });
 	        
@@ -145,7 +114,7 @@ public class ApprovalReportsController  implements Initializable {
         cityApprovalDisapproval.setCellFactory(new Callback<TableColumn<CitySubmission, Button>, TableCell<CitySubmission, Button>>() {
             @Override
             public TableCell<CitySubmission, Button> call(TableColumn<CitySubmission, Button> param) {
-                return new CityTableCell(gcmDAO);
+                return new CityTableCell();
             }
         });
         
@@ -161,32 +130,14 @@ public class ApprovalReportsController  implements Initializable {
         tourApprovalDisapproval.setCellFactory(new Callback<TableColumn<TourSubmission, Button>, TableCell<TourSubmission, Button>>() {
             @Override
             public TableCell<TourSubmission, Button> call(TableColumn<TourSubmission, Button> param) {
-                return new TourTableCell(gcmDAO);
+                return new TourTableCell();
             }
         });
 
         ObservableList<TourSubmission> details = tourSubmission.isEmpty() ? FXCollections.observableArrayList() : FXCollections.observableArrayList(tourSubmission);
         tourTable.setItems(details);
 	}
-
-	public void initMapTableView() {
-        mapName.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getMap().getName()));
-        mapDescription.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getMap().getDescription()));
-        mapPrice.setCellValueFactory(data ->  new ReadOnlyStringWrapper(Double.toString(data.getValue().getMap().getPrice())));
-        mapActionTaken.setCellValueFactory(data ->  new ReadOnlyStringWrapper(data.getValue().getActionTaken()));
-        
-        mapApprovalDisapproval.setCellFactory(new Callback<TableColumn<MapSubmission, Button>, TableCell<MapSubmission, Button>>() {
-            @Override
-            public TableCell<MapSubmission, Button> call(TableColumn<MapSubmission, Button> param) {
-                return new MapTableCell(gcmDAO);
-            }
-        });
-        
-        
-        ObservableList<MapSubmission> details = FXCollections.observableArrayList(mapSubmission);
-        mapTable.setItems(details);
-	}
-
+	
 	
 	public void cityBtnListener() {
 		cityReports.setOnMouseClicked((new EventHandler<MouseEvent>() {
@@ -196,8 +147,6 @@ public class ApprovalReportsController  implements Initializable {
 				cityTable.setVisible(true);
 				siteTable.setVisible(false);
 				tourTable.setVisible(false);
-				mapTable.setVisible(false);
-
 			}
 			
 		}));
@@ -210,8 +159,6 @@ public class ApprovalReportsController  implements Initializable {
 				siteTable.setVisible(true);
 				cityTable.setVisible(false);
 				tourTable.setVisible(false);
-				mapTable.setVisible(false);
-
 			}
 			
 		}));
@@ -224,47 +171,25 @@ public class ApprovalReportsController  implements Initializable {
 				siteTable.setVisible(false);
 				cityTable.setVisible(false);
 				tourTable.setVisible(true);
-				mapTable.setVisible(false);
-
 			}
 			
 		}));
 	}
-	public void mapBtnListener() {
-		mapReports.setOnMouseClicked((new EventHandler<MouseEvent>() {
-			
-			@Override
-			public void handle(MouseEvent arg0) {
-				siteTable.setVisible(false);
-				cityTable.setVisible(false);
-				tourTable.setVisible(false);
-				mapTable.setVisible(true);
-			}
-			
-		}));
-	}	
-
 	
 	public void initTables() {
 		cityTable.setVisible(false);
 		siteTable.setVisible(false);
 		tourTable.setVisible(false);
-		mapTable.setVisible(false);
-		
-		initCityTableView();
-		initSiteTableView();
-		initTourTableView();
-		initMapTableView();
-
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initTables();
-		
+		initCityTableView();
+		initSiteTableView();
+		initTourTableView();
 		siteBtnListener();
 		cityBtnListener();
 		tourBtnListener();
-		mapBtnListener();
 	}
 	
 	public void initialize() {
@@ -282,62 +207,14 @@ public class ApprovalReportsController  implements Initializable {
 		GcmDAO gcmDAO = gcmClient.getDataAccessObject();
 		List<CitySubmission> citySubmissions = fetchCitySubmissions(gcmDAO); 
 		List<SiteSubmission> siteSubmissions = fetchSiteSubmissions(gcmDAO);
-		List<MapSubmission> mapSubmissions = fetchMapSubmissions(gcmDAO);
-		List<TourSubmission> tourSubmissions = fetchTourSubmissions(gcmDAO);
 		
 		return new ApprovalReportsController(gcmClient, 
 				gcmDAO, 
 				citySubmissions, 
 				siteSubmissions, 
-				tourSubmissions,
-				mapSubmissions);
+				new ArrayList<TourSubmission>());
 	}
-	private static List<TourSubmission> fetchTourSubmissions(GcmDAO gcmDAO) {
-		List<TourSubmission> mapSubmissions = new ArrayList<TourSubmission>();
-		List<Tour> tourAdded = gcmDAO.getToursAddEdits();
-		List<Tour> tourModified = gcmDAO.getToursUpdateEdits();
-		List<Tour> tourDeleted = gcmDAO.getToursDeleteEdits();
 
-		if(tourAdded != null && !tourAdded.isEmpty()) {
-			for (Tour tour : tourAdded) {
-				mapSubmissions.add(new TourSubmission(tour, ActionTaken.ADD));
-			}
-		}
-		if(tourModified!= null && !tourModified.isEmpty()) {
-			for (Tour tour : tourModified) {
-				mapSubmissions.add(new TourSubmission(tour, ActionTaken.UPDATE));
-			}
-		} 
-		if(tourDeleted!= null && !tourDeleted.isEmpty()) {
-			for (Tour tour : tourDeleted) {
-				mapSubmissions.add(new TourSubmission(tour, ActionTaken.DELETE));
-			}
-		}
-		return mapSubmissions;
-	}
-	private static List<MapSubmission> fetchMapSubmissions(GcmDAO gcmDAO) {
-		List<MapSubmission> mapSubmissions = new ArrayList<MapSubmission>();
-		List<Map> mapsAdded = gcmDAO.getMapsAddEdits();
-		List<Map> mapsModified = gcmDAO.getMapsUpdateEdits();
-		List<Map> mapsDeleted = gcmDAO.getMapsDeleteEdits();
-
-		if(mapsAdded != null && !mapsAdded.isEmpty()) {
-			for (Map map : mapsAdded) {
-				mapSubmissions.add(new MapSubmission(map, ActionTaken.ADD));
-			}
-		}
-		if(mapsModified!= null && !mapsModified.isEmpty()) {
-			for (Map map : mapsModified) {
-				mapSubmissions.add(new MapSubmission(map, ActionTaken.UPDATE));
-			}
-		} 
-		if(mapsDeleted!= null && !mapsDeleted.isEmpty()) {
-			for (Map map : mapsDeleted) {
-				mapSubmissions.add(new MapSubmission(map, ActionTaken.DELETE));
-			}
-		}
-		return mapSubmissions;
-	}
 	private static List<CitySubmission> fetchCitySubmissions(GcmDAO gcmDAO) {
 		List<CitySubmission> citySubmissions = new ArrayList<CitySubmission>();
 		List<City> citiesAdded = gcmDAO.getCitiesAddEdits();
@@ -350,7 +227,7 @@ public class ApprovalReportsController  implements Initializable {
 		}
 		if(citiesModified!= null && !citiesModified.isEmpty()) {
 			for (City city : citiesModified) {
-				citySubmissions.add(new CitySubmission(city, ActionTaken.UPDATE));
+				citySubmissions.add(new CitySubmission(city, ActionTaken.EDIT));
 			}
 		} 
 		if(citiesDeleted!= null && !citiesDeleted.isEmpty()) {
@@ -373,7 +250,7 @@ public class ApprovalReportsController  implements Initializable {
 		}
 		if(sitesModified!= null && !sitesModified.isEmpty()) {
 			for (Site site : sitesModified) {
-				siteSubmissions.add(new SiteSubmission(site, ActionTaken.UPDATE));
+				siteSubmissions.add(new SiteSubmission(site, ActionTaken.EDIT));
 			}
 		} 
 		if(sitesDeleted!= null && !sitesDeleted.isEmpty()) {
@@ -384,4 +261,3 @@ public class ApprovalReportsController  implements Initializable {
 		return siteSubmissions;
 	}
 }
-
