96d72fed8e09070c6db81657af8f72429ab454d1
eliyahulevi <aoliko54@gmail.com>
2019-06-22 13:26:16 +0300

    fixed user presntation and adding some nedded fitcher

--- GCM/App/userDetailsPresentation/UserDetailsPresentationController.java
+++ GCM/App/userDetailsPresentation/UserDetailsPresentationController.java
@@ -2,32 +2,47 @@ package userDetailsPresentation;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import dataAccess.customer.PurchaseHistory;
-import gcmDataAccess.GcmDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.text.Text;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import mainApp.GcmClient;
 import users.User;
+import utility.TextFieldUtility;
 
 public class UserDetailsPresentationController implements Initializable {
 
 	private User user;
 	private List<PurchaseHistory> purchaseHistories;
 	private GcmClient gcmClient;
+	private final int MAX_PASSWORD_LENGTH = 12;
+	private final int MIN_PASSWOR_LENGTH = 6;
+	private final int MAX_USERNAME_LENGTH = 10;
+	private final int MIN_USERNAME_LENGTH = 4;
+	private boolean flag = true;
 	// private GcmDAO gcmDAO;
 
 	@FXML
-	private Text nametxt, lastnametxt, emailtxt, phonenumbertxt, usernametxt;
+	private TextField nametf, lastnametf, emailtf, phonenumbertf, usernametf;
+	@FXML
+	private PasswordField passwordtf, confirmpasswordtf;
 	@FXML
 	private ListView<String> listviewLV;
+	@FXML
+	private Button editbtn, updatebtn;
+	@FXML
+	private Label passwordlabel, confirmpasswordlabel, changelabel;
 
 	public UserDetailsPresentationController(GcmClient gcmClient) {
 		this.gcmClient = gcmClient;
@@ -38,13 +53,89 @@ public class UserDetailsPresentationController implements Initializable {
 		gcmClient.back();
 	}
 
+	@FXML
+	public void onEditButton() {
+
+		updatebtn.setVisible(true);
+		emailtf.setEditable(true);
+		phonenumbertf.setEditable(true);
+		passwordlabel.setVisible(true);
+		confirmpasswordlabel.setVisible(true);
+		passwordtf.setVisible(true);
+		confirmpasswordtf.setVisible(true);
+		passwordtf.setEditable(true);
+		confirmpasswordtf.setEditable(true);
+		changelabel.setVisible(true);
+		usernametf.setEditable(true);
+
+	}
+
+	@FXML
+	public void onUpdateButton(ActionEvent event) {
+		if (!TextFieldUtility.validEmail(emailtf.getText())) {
+			TextFieldUtility.ShowAlert(AlertType.ERROR, TextFieldUtility.getStageWindow(event), "Form Error!",
+					"invalid email address");
+		} else if (!validpassword(passwordtf.getText())) {
+			TextFieldUtility.ShowAlert(AlertType.ERROR, TextFieldUtility.getStageWindow(event), "Form Error!",
+					"password length need to be between 6 to 10 characters");
+		} else if (!validConfirmPassword(passwordtf.getText(), confirmpasswordtf.getText())) {
+			TextFieldUtility.ShowAlert(AlertType.ERROR, TextFieldUtility.getStageWindow(event), "Form Error!",
+					"password and confirm password dont match");
+		} else if (!Validusername(usernametf.getText())) {
+			TextFieldUtility.ShowAlert(AlertType.ERROR, TextFieldUtility.getStageWindow(event), "Form Error!",
+					"username length need to be between 4 to 10 characters");
+		} else {
+
+			// build new user and send and the new password
+			User userToUpdate = new User(usernametf.getText(), user.getFirstName(), user.getLastName(),
+					emailtf.getText(), phonenumbertf.getText());
+
+			// after check with amit about this funtions
+
+			// need to send update in dataBase
+			// if(!passwordtf.getText().equals("")) {
+			// if(!gcmClient.updateUser(userToUpdate, passwordtf.getText()) {
+			// TextFieldUtility.ShowAlert(AlertType.ERROR,
+			// TextFieldUtility.getStageWindow(event), "invalid username","Sorry, Username
+			// already exist plaese chose another one");
+			// flag =fale;
+			// }
+			// } else {
+			// // need to send update in dataBase
+			// if(!gcmClient.updateUser(userToUpdate)) {
+			// TextFieldUtility.ShowAlert(AlertType.ERROR,
+			// TextFieldUtility.getStageWindow(event), "invalid username","Sorry, Username
+			// already exist plaese chose another one");
+			// flag = false;
+			// }
+			// }
+			if (flag) {
+				updatebtn.setVisible(false);
+				emailtf.setEditable(false);
+				phonenumbertf.setEditable(false);
+				passwordlabel.setVisible(false);
+				confirmpasswordlabel.setVisible(false);
+				passwordtf.setVisible(false);
+				confirmpasswordtf.setVisible(false);
+				passwordtf.setEditable(false);
+				confirmpasswordtf.setEditable(false);
+				changelabel.setVisible(false);
+				usernametf.setEditable(false);
+			}
+
+			flag = true;
+
+		}
+
+	}
+
 	public void setDeteails() {
 		user = gcmClient.getUserInfo().getUserDetailes();
-		nametxt.setText(user.getFirstName());
-		lastnametxt.setText(user.getLastName());
-		emailtxt.setText(user.getEmail());
-		phonenumbertxt.setText(user.getPhoneNumber());
-		usernametxt.setText(user.getUsername());
+		nametf.setText(user.getFirstName());
+		lastnametf.setText(user.getLastName());
+		emailtf.setText(user.getEmail());
+		phonenumbertf.setText(user.getPhoneNumber());
+		usernametf.setText(user.getUsername());
 
 		List<PurchaseHistory> purchaseHistories = gcmClient.getDataAccessObject().getPurchaseHistory();
 		List<String> cityPurchaseStrings = new ArrayList<>();
@@ -59,10 +150,39 @@ public class UserDetailsPresentationController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// setDeteails();
+		TextFieldUtility.numericTextOnly(phonenumbertf);
+		TextFieldUtility.addTextLimiter(phonenumbertf, 10);
 	}
 
 	public void initalizeControl() {
 		setDeteails();
+	}
+
+	public boolean validpassword(String password) {
+
+		if (password.length() == 0) {
+			return true;
+		}
+
+		if (password.length() > MAX_PASSWORD_LENGTH || password.length() < MIN_PASSWOR_LENGTH) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean validConfirmPassword(String password, String confirmPassword) {
+		if (!password.equals(confirmPassword)) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean Validusername(String username) {
+		if (username.length() > MAX_USERNAME_LENGTH || username.length() < MIN_USERNAME_LENGTH) {
+			return false;
+		}
+
+		return true;
 	}
 
 }
