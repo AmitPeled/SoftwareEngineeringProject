b0b458f6e3022bceb0c846549e1ce974bc5e432e
Amit Peled <amit.peled0@gmail.com>
2019-06-24 10:11:43 +0300

    manager reports communication

--- GCM/CommunicationObjects/queries/GcmQuery.java
+++ GCM/CommunicationObjects/queries/GcmQuery.java
@@ -3,17 +3,19 @@ package queries;
 import java.io.Serializable;
 
 public enum GcmQuery implements Serializable {
-	
-	addCustomer, verifyUser, addMap, deleteMap, getMapDetails, getMapFile, addCity, deleteCity, addNewSiteToCity,
-	addExistingSiteToMap, deleteSiteFromMap, getMapsByCityName, getMapsBySiteName, getMapsByDescription, getUserDetails,
-	getCityByMapId, purchaseCity, getActiveCitiesPurchases, downloadMap, addNewTourToCity, addExistingTourToMap,
-	addExistingSiteToTour, getCitySites, purchaseMembershipToCity, getToursDeleteEdits, getToursUpdateEdits,
-	getToursAddEdits, getToursObjectAddedTo, getCitiesObjectAddedTo, getMapsObjectAddedTo, getSitesAddEdits,
-	getCitiesDeleteEdits, getCitiesUpdateEdits, getCitiesAddEdits, getSitesDeleteEdits, getSitesUpdateEdits,
-	getMapsDeleteEdits, getMapsUpdateEdits, getMapsAddEdits, actionSiteDeleteEdit, actionSiteUpdateEdit,
-	actionSiteAddEdit, actionCityDeleteEdit, actionCityUpdateEdit, actionCityAddEdit, actionMapAddEdit,
-	actionMapUpdateEdit, actionMapDeleteEdit, getSavedCreditCard, notifyMapView, editCityPrice, getSiteSubmissions,
-	getTourSubmissions, getMapSubmissions, actionSiteEdit, actionMapEdit, actionTourEdit, actionCityEdit, updateMap,
-	deleteSiteFromCity, updateTour, getCityTours, deleteTourFromCity, deleteTourFromMap, UpdateSite, deleteCityEdit,
-	updateCity, changeCityPrices, getPriceSubmissions, approveCityPrice, getPurchaseHistory, getCity, logout, getTour, getCitySubmissions, editUsersWithNewPassword, editUsersWithoutNewPassword
+
+	 addCustomer, verifyUser, addMap, deleteMap, getMapDetails, getMapFile, addCity, deleteCity, addNewSiteToCity,
+	 addExistingSiteToMap, deleteSiteFromMap, getMapsByCityName, getMapsBySiteName, getMapsByDescription,
+	 getUserDetails, getCityByMapId, purchaseCity, getActiveCitiesPurchases, downloadMap, addNewTourToCity,
+	 addExistingTourToMap, addExistingSiteToTour, getCitySites, purchaseMembershipToCity, getToursDeleteEdits,
+	 getToursUpdateEdits, getToursAddEdits, getToursObjectAddedTo, getCitiesObjectAddedTo, getMapsObjectAddedTo,
+	 getSitesAddEdits, getCitiesDeleteEdits, getCitiesUpdateEdits, getCitiesAddEdits, getSitesDeleteEdits,
+	 getSitesUpdateEdits, getMapsDeleteEdits, getMapsUpdateEdits, getMapsAddEdits, actionSiteDeleteEdit,
+	 actionSiteUpdateEdit, actionSiteAddEdit, actionCityDeleteEdit, actionCityUpdateEdit, actionCityAddEdit,
+	 actionMapAddEdit, actionMapUpdateEdit, actionMapDeleteEdit, getSavedCreditCard, notifyMapView, editCityPrice,
+	 getSiteSubmissions, getTourSubmissions, getMapSubmissions, actionSiteEdit, actionMapEdit, actionTourEdit,
+	 actionCityEdit, updateMap, deleteSiteFromCity, updateTour, getCityTours, deleteTourFromCity, deleteTourFromMap,
+	 UpdateSite, deleteCityEdit, updateCity, changeCityPrices, getPriceSubmissions, approveCityPrice,
+	 getPurchaseHistory, getCity, logout, getTour, getCitySubmissions, editUsersWithNewPassword,
+	 editUsersWithoutNewPassword, getSystemReport, getCityReport
 }
